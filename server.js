a9b3dc4a937b1eeac69b53334b42ef862c6af638
AnanyaThisSide <mail4mananya@gmail.com>
2023-01-20 00:50:38 +0000

    cors added

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 import express from "express";
+import cors from "cors";
 
 const PORT = 8000;
 
 const app = express();
+
+app.use(cors({
+    origin: ['http://localhost:4200','http://mean-ecom.s3-website-us-east-1.amazonaws.com'],
+    credentials: true
+}))
+
 app.use('/name/:id', (req, res, next) => {
     res.send('Welcome '+req.params.id+'!!');
 });
